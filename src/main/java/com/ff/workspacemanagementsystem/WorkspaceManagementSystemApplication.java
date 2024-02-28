c4cb7628f632d5f51c8e947f4541ee00f563d178
thejuthunga <thejasthejashree@gmail.com>
2024-02-28 12:35:34 +0530

    First Commit

---
+++ src/main/java/com/ff/workspacemanagementsystem/WorkspaceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ff.workspacemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ff/workspacemanagementsystem/WorkspaceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.workspacemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
