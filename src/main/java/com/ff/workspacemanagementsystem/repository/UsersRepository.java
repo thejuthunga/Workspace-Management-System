bc2950903c87af28eefed9b80af51a8b81151675
thejuthunga <thejasthejashree@gmail.com>
2024-02-28 16:00:02 +0530

    Third commit

---
+++ src/main/java/com/ff/workspacemanagementsystem/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.ff.workspacemanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ff.workspacemanagementsystem.entity.Users;
+
+public interface UsersRepository extends JpaRepository<Users, Integer>{
+
+}
---
+++ src/main/java/com/ff/workspacemanagementsystem/service/UsersService.java
@@ -0,0 +1,8 @@
+package com.ff.workspacemanagementsystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+
+}
