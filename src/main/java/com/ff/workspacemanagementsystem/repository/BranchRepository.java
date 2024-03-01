c76cf3d787bb5ee06bde3469f4c7401ac08550ff
thaslim-mubarak <thaslimmubarakb@gmail.com>
2024-03-01 10:35:12 +0530

    Merged

--- src/main/java/com/ff/workspacemanagementsystem/repository/BranchRepository.java
+++ src/main/java/com/ff/workspacemanagementsystem/repository/BranchRepository.java
@@ -7,8 +7,5 @@ import com.ff.workspacemanagementsystem.entity.Branch;
 
 public interface BranchRepository extends JpaRepository<Branch, Integer>{
 
-	@Query("select b.floors_count from branch b where b.branch_id = ?1")
-	public int getFloorCount(int b_id);
-
 
 }
